7412d383f73f0c438752e4c68acc37afe8576955
laike9m <laike9m@gmail.com>
2014-01-10 02:27:48 +0800

    fix bugs

--- css3two_blog/models.py
+++ css3two_blog/models.py
@@ -9,13 +9,13 @@ from django.template.defaultfilters import slugify
 from django.core.urlresolvers import reverse
 from unidecode import unidecode
 
-# delete md_file before delete model
-from django.db.models.signals import pre_delete
+# delete md_file before delete/change model
+from django.db.models.signals import pre_delete, pre_save
 from django.dispatch import receiver
 
 # get gfm html and store it
 from django.conf import settings
-from os.path import join
+from os.path import splitext
 import requests
 
 
@@ -25,6 +25,7 @@ upload_dir = 'content/BlogPost/%s/%s'
 def get_upload_file_name(instance, filename):
     year = date.today().year    # 按照年份存放
     upload_to = upload_dir % (year, instance.title + '.md')
+    instance.abspath_upload_to = settings.MEDIA_ROOT + '/' + upload_to
     return upload_to
 
 
@@ -36,7 +37,11 @@ class BlogPost(models.Model):
     pub_date = models.DateTimeField('date published', default=timezone.now())
     last_edit_date = models.DateTimeField('last edited', default=timezone.now())
     slug = models.SlugField(blank=True)
-    
+
+    def __init__(self, *args, **kwargs):
+        self.abspath_upload_to = ''
+        super().__init__(*args, **kwargs)
+
     def __str__(self):
         return self.title   # 根据继承搜索流程,先是实例属性,然后就是类属性,所以这样用没问题
 
@@ -53,15 +58,12 @@ class BlogPost(models.Model):
             self.body = self.md_file.read()   # bytes !
 
         # generate rendered html file with same name as md
-        #if exists 同名html文件 delete
         data = str(self.body)[2:-1].encode('utf-8').decode('unicode_escape')
         headers = {'Content-Type': 'text/plain'}
         r = requests.post('https://api.github.com/markdown/raw', headers=headers, data=data)
-        media_root = settings.MEDIA_ROOT
-        year = date.today().year
-        self.html_filename = join(media_root, upload_dir % (year, self.title + '.html'))
+        self.html_filename = splitext(self.abspath_upload_to)[0] + '.html'
+        print(self.html_filename)
         with open(self.html_filename, 'wt') as f:
-            print(r.text)
             f.write(r.text)
 
         super(BlogPost, self).save(*args, **kwargs)
