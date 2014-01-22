79f100c2715f9343cad35aa8512024f72230d592
laike9m <laike9m@gmail.com>
2014-01-23 00:40:08 +0800

    archive logic done

--- css3two_blog/views.py
+++ css3two_blog/views.py
@@ -3,6 +3,7 @@ from  django.http import HttpResponse
 from django.core.context_processors import csrf
 from django.core.urlresolvers import reverse
 from css3two_blog.models import BlogPost
+from collections import defaultdict
 
 # Create your views here.
 def home(request):
@@ -16,15 +17,30 @@ def blogpost(request, slug, id):
     blogpost = get_object_or_404(BlogPost, pk=id)
     args['blogpost'] = blogpost
     return render(request, 'css3two_blog/blogpost.html', args)
-    
+
 
 def archive(request):
     args = dict()
+
+    def get_sorted_posts(category):
+        posts_by_year = defaultdict(list)
+        posts_of_a_category = BlogPost.objects.filter(category=category)  # already sorted by pub_date
+        for post in posts_of_a_category:
+            year = post.pub_date.year
+            posts_by_year[year].append(post)  # {'2013':post_list, '2014':post_list}
+        posts_by_year = sorted(posts_by_year.items(), reverse=True)  # [('2014',post_list), ('2013',post_list)]
+        return posts_by_year
+
     args['data'] = [
-        ('acg', BlogPost.objects.filter(category="acg")),
-        ('programming', BlogPost.objects.filter(category="programming")),
-        ('nc', BlogPost.objects.filter(category="nc")),   # no category
+        ('programming', get_sorted_posts(category="programming")),
+        ('acg', get_sorted_posts(category="acg")),
+        ('nc', get_sorted_posts(category="programming")),   # no category
     ]
+
+    args['MONTH'] = ('JAN', 'FEB', 'MAR', 'APR', 'MAY', 'JUN', 'JUL', 'AUG', 'SEP', 'OCT', 'NOV', 'DEC')
+
+    from pprint import pprint
+    pprint(args['data'])
     return render(request, 'css3two_blog/archive.html', args)
 
 
