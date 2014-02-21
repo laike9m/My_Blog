bd6712706a8f45d69ba2a6c92699635f51056273
laike9m <laike9m@gmail.com>
2014-02-21 07:00:30 +0000

    modified settings.py

--- my_blog/settings.py
+++ my_blog/settings.py
@@ -133,6 +133,7 @@ INSTALLED_APPS = (
     'mytemplatetags',
 )
 
+
 # A sample logging configuration. The only tangible logging
 # performed by this configuration is to send an email to
 # the site admins on every HTTP 500 error when DEBUG=False.
