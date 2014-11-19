35675f668a00114bdc75a751693ab28960418243
laike9m <laike9m@gmail.com>
2014-11-19 13:16:48 +0800

    use id then

--- css3two_blog/urls.py
+++ css3two_blog/urls.py
@@ -5,7 +5,7 @@ from .feeds import BlogPostFeed
 urlpatterns = patterns(
     '',
     url('^$', views.home),
-    url(r'^(?P<slug>[-\w\d]+),(?P<post_id>\d+)/$', views.blogpost),
+    url(r'^(?P<slug>[-\w\d]+),(?P<id>\d+)/$', views.blogpost),
     url('^archive/$', views.archive),
     url('^about/$', views.about),
     url('^projects/$', views.projects),
