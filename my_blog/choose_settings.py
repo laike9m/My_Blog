181eb8e30127b0383d6d522faf95f68673fdd837
laike9m <laike9m@gmail.com>
2015-10-07 03:12:34 -0400

    uwsgi autoreload when Django code changes

--- my_blog/choose_settings.py
+++ my_blog/choose_settings.py
@@ -67,3 +67,11 @@ else:
         }
     }
 
+    import uwsgi
+    from uwsgidecorators import timer
+    from django.utils import autoreload
+
+    @timer(3)
+    def change_code_gracefull_reload(sig):
+        if autoreload.code_changed():
+            uwsgi.reload()
