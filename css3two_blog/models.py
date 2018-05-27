4c2bde90da3bc589281ba327b3ecb82c9dcfea2b
laike9m <laike9m@gmail.com>
2018-05-28 00:40:20 +0800

    Use header-ids and toc in markdown2

--- css3two_blog/models.py
+++ css3two_blog/models.py
@@ -17,18 +17,17 @@ import markdown2
 from unidecode import unidecode
 from taggit.managers import TaggableManager
 
-
 upload_dir = 'content/BlogPost/%s/%s'
 
 
 class BlogPost(models.Model):
-
     class Meta:
-        ordering = ['-pub_date']    # ordered by pub_date descending when retriving
+        # ordered by pub_date descending when retriving
+        ordering = ['-pub_date']
 
     def get_upload_md_name(self, filename):
         if self.pub_date:
-            year = self.pub_date.year   # always store in pub_year folder
+            year = self.pub_date.year  # always store in pub_year folder
         else:
             year = datetime.now().year
         upload_to = upload_dir % (year, self.title + '.md')
@@ -50,17 +49,19 @@ class BlogPost(models.Model):
 
     title = models.CharField(max_length=150)
     body = models.TextField(blank=True)
-    md_file = models.FileField(upload_to=get_upload_md_name, blank=True)  # uploaded md file
+    # uploaded md file
+    md_file = models.FileField(upload_to=get_upload_md_name, blank=True)
     pub_date = models.DateTimeField('date published', auto_now_add=True)
     last_edit_date = models.DateTimeField('last edited', auto_now=True)
     slug = models.SlugField(max_length=200, blank=True)
-    html_file = models.FileField(upload_to=get_html_name, blank=True)    # generated html file
+    # generated html file
+    html_file = models.FileField(upload_to=get_html_name, blank=True)
     category = models.CharField(max_length=30, choices=CATEGORY_CHOICES)
     description = models.TextField(blank=True)
     tags = TaggableManager()
 
     def __str__(self):
-        return self.title   # 根据继承搜索流程,先是实例属性,然后就是类属性,所以这样用没问题
+        return self.title  # 根据继承搜索流程,先是实例属性,然后就是类属性,所以这样用没问题
 
     @property
     def filename(self):
@@ -75,9 +76,12 @@ class BlogPost(models.Model):
             self.body = self.md_file.read()
 
         html = markdown2.markdown(self.body,
-                                  extras=["fenced-code-blocks", "tables"])
+                                  extras=["fenced-code-blocks", "tables", "toc",
+                                          "header-ids"])
         self.html_file.save(self.title + '.html',
-                            ContentFile(html.encode('utf-8')), save=False)
+                            ContentFile(html.toc_html.encode('utf-8') +
+                                        html.encode('utf-8')),
+                            save=False)
         self.html_file.close()
 
         super().save(*args, **kwargs)
@@ -102,9 +106,9 @@ def blogpost_delete(instance, **kwargs):
 class BlogPostImage(models.Model):
 
     def get_upload_img_name(self, filename):
-        upload_to = upload_dir % ('images', filename)  # filename involves extension
+        # filename involves extension
+        upload_to = upload_dir % ('images', filename)
         return upload_to
 
     blogpost = models.ForeignKey(BlogPost, related_name='images')
     image = models.ImageField(upload_to=get_upload_img_name)
-
