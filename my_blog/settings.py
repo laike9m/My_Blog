a581daa8a73a22db09e3a8576f38289675a1f6e7
laike9m <laike9m@gmail.com>
2024-08-26 02:24:51 -0700

    Add django_extensions to apps

--- my_blog/settings.py
+++ my_blog/settings.py
@@ -2,6 +2,7 @@
 import os
 
 from dotenv import load_dotenv
+
 load_dotenv()
 
 from . import choose_settings
@@ -13,7 +14,7 @@ ALLOWED_HOSTS = choose_settings.ALLOWED_HOSTS
 
 ADMINS = (
     # ('Your Name', 'your_email@example.com'),
-    ('laike9m', 'laike9m@gmail.com'),
+    ("laike9m", "laike9m@gmail.com"),
 )
 
 MANAGERS = ADMINS
@@ -26,11 +27,11 @@ DATABASES = choose_settings.DATABASES
 # http://en.wikipedia.org/wiki/List_of_tz_zones_by_name
 # although not all choices may be available on all operating systems.
 # In a Windows environment this must be set to your system time zone.
-TIME_ZONE = 'Asia/Shanghai'
+TIME_ZONE = "Asia/Shanghai"
 
 # Language code for this installation. All choices can be found here:
 # http://www.i18nguy.com/unicode/language-identifiers.html
-LANGUAGE_CODE = 'en-us'
+LANGUAGE_CODE = "en-us"
 
 SITE_ID = 1
 
@@ -73,44 +74,43 @@ STATIC_URL = choose_settings.STATIC_URL
 # List of finder classes that know how to find static files in
 # various locations.
 STATICFILES_FINDERS = (
-    'django.contrib.staticfiles.finders.FileSystemFinder',
-    'django.contrib.staticfiles.finders.AppDirectoriesFinder',
+    "django.contrib.staticfiles.finders.FileSystemFinder",
+    "django.contrib.staticfiles.finders.AppDirectoriesFinder",
     # 'django.contrib.staticfiles.finders.DefaultStorageFinder',
 )
 
 # Make this unique, and don't share it with anybody.
-SECRET_KEY = 'v2x^#lrv$(xp3ost97tbr4wvodd6l6obm_f3s%a^6pdmpxhw=g'
+SECRET_KEY = "v2x^#lrv$(xp3ost97tbr4wvodd6l6obm_f3s%a^6pdmpxhw=g"
 
 # List of callables that know how to import templates from various sources.
 TEMPLATE_LOADERS = (
-    'django.template.loaders.filesystem.Loader',
-    'django.template.loaders.app_directories.Loader',
+    "django.template.loaders.filesystem.Loader",
+    "django.template.loaders.app_directories.Loader",
     # 'django.template.loaders.eggs.Loader',
 )
 
 MIDDLEWARE = (
-    'django.middleware.common.CommonMiddleware',
-    'django.contrib.sessions.middleware.SessionMiddleware',
-    'django.middleware.csrf.CsrfViewMiddleware',
-    'django.contrib.auth.middleware.AuthenticationMiddleware',
-    'django.contrib.messages.middleware.MessageMiddleware',
-    'django.middleware.clickjacking.XFrameOptionsMiddleware',
+    "django.middleware.common.CommonMiddleware",
+    "django.contrib.sessions.middleware.SessionMiddleware",
+    "django.middleware.csrf.CsrfViewMiddleware",
+    "django.contrib.auth.middleware.AuthenticationMiddleware",
+    "django.contrib.messages.middleware.MessageMiddleware",
+    "django.middleware.clickjacking.XFrameOptionsMiddleware",
 )
 
-if 'CACHES' in dir(choose_settings):
+if "CACHES" in dir(choose_settings):
     CACHES = choose_settings.CACHES
 
-if 'MIDDLEWARE_ADDITION_FIRST' in dir(choose_settings):
-    MIDDLEWARE = choose_settings.MIDDLEWARE_ADDITION_FIRST \
-                        + MIDDLEWARE
+if "MIDDLEWARE_ADDITION_FIRST" in dir(choose_settings):
+    MIDDLEWARE = choose_settings.MIDDLEWARE_ADDITION_FIRST + MIDDLEWARE
 
-if 'MIDDLEWARE_ADDITION_LAST' in dir(choose_settings):
+if "MIDDLEWARE_ADDITION_LAST" in dir(choose_settings):
     MIDDLEWARE += choose_settings.MIDDLEWARE_ADDITION_LAST
 
-ROOT_URLCONF = 'my_blog.urls'
+ROOT_URLCONF = "my_blog.urls"
 
 # Python dotted path to the WSGI application used by Django's runserver.
-WSGI_APPLICATION = 'my_blog.wsgi.application'
+WSGI_APPLICATION = "my_blog.wsgi.application"
 
 TEMPLATE_DIRS = choose_settings.TEMPLATE_DIRS
 # Put strings here, like "/home/html/django_templates" or "C:/www/django/templates".
@@ -118,21 +118,22 @@ TEMPLATE_DIRS = choose_settings.TEMPLATE_DIRS
 # Don't forget to use absolute paths, not relative paths.
 
 INSTALLED_APPS = (
-    'django.contrib.auth',
-    'django.contrib.contenttypes',
-    'django.contrib.sessions',
-    'django.contrib.sites',
-    'django.contrib.messages',
-    'django.contrib.staticfiles',
+    "django_extensions",
+    "django.contrib.auth",
+    "django.contrib.contenttypes",
+    "django.contrib.sessions",
+    "django.contrib.sites",
+    "django.contrib.messages",
+    "django.contrib.staticfiles",
     # Uncomment the next line to enable the admin:
-    'django.contrib.admin',
+    "django.contrib.admin",
     # Uncomment the next line to enable admin documentation:
-    'django.contrib.admindocs',
-    'css3two_blog',
-    'taggit',
-    'mytemplatetags',
-    'contact_form',
-    #"compressor",
+    "django.contrib.admindocs",
+    "css3two_blog",
+    "taggit",
+    "mytemplatetags",
+    "contact_form",
+    # "compressor",
 )
 
 
@@ -142,40 +143,36 @@ INSTALLED_APPS = (
 # See http://docs.djangoproject.com/en/dev/topics/logging for
 # more details on how to customize your logging configuration.
 LOGGING = {
-    'version': 1,
-    'disable_existing_loggers': False,
-    'filters': {
-        'require_debug_false': {
-            '()': 'django.utils.log.RequireDebugFalse'
+    "version": 1,
+    "disable_existing_loggers": False,
+    "filters": {"require_debug_false": {"()": "django.utils.log.RequireDebugFalse"}},
+    "handlers": {
+        "mail_admins": {
+            "level": "ERROR",
+            "filters": ["require_debug_false"],
+            "class": "django.utils.log.AdminEmailHandler",
         }
     },
-    'handlers': {
-        'mail_admins': {
-            'level': 'ERROR',
-            'filters': ['require_debug_false'],
-            'class': 'django.utils.log.AdminEmailHandler'
-        }
-    },
-    'loggers': {
-        'django.request': {
-            'handlers': ['mail_admins'],
-            'level': 'ERROR',
-            'propagate': True,
+    "loggers": {
+        "django.request": {
+            "handlers": ["mail_admins"],
+            "level": "ERROR",
+            "propagate": True,
         },
-    }
+    },
 }
 
 TEMPLATES = [
     {
-        'BACKEND': 'django.template.backends.django.DjangoTemplates',
-        'DIRS': [],
-        'APP_DIRS': True,
-        'OPTIONS': {
-            'context_processors': [
-                'django.template.context_processors.debug',
-                'django.template.context_processors.request',
-                'django.contrib.auth.context_processors.auth',
-                'django.contrib.messages.context_processors.messages',
+        "BACKEND": "django.template.backends.django.DjangoTemplates",
+        "DIRS": [],
+        "APP_DIRS": True,
+        "OPTIONS": {
+            "context_processors": [
+                "django.template.context_processors.debug",
+                "django.template.context_processors.request",
+                "django.contrib.auth.context_processors.auth",
+                "django.contrib.messages.context_processors.messages",
             ],
         },
     },
@@ -184,7 +181,7 @@ TEMPLATES = [
 
 if not choose_settings.is_dev_machine():
     EMAIL_USE_TLS = True
-    EMAIL_HOST = 'smtp.mailgun.org'
+    EMAIL_HOST = "smtp.mailgun.org"
     EMAIL_PORT = 587
     EMAIL_HOST_USER = os.environ["EMAIL_HOST_USER"]
     EMAIL_HOST_PASSWORD = os.environ["EMAIL_HOST_PASSWORD"]
